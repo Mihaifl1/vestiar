f35dd9c6d3a558fcf0cccad10080b651fbb0dfed
Mihaifl1 <98021391+Mihaifl1@users.noreply.github.com>
2025-09-28 18:02:49 +0300

    index_card.html

--- index_card.php
+++ index_card.php
@@ -1,6 +1,6 @@
 <!doctype html>
 <html lang="ro">
-<head>
+<head> 
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1" />
   <title>Vestiar — Control lacăte</title>
